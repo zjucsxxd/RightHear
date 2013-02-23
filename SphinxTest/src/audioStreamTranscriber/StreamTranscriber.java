7d1255e568b26c4446d58d34670a0375dc613ac8
Taylor Wrobel <taylor.wrobel@gmail.com>
2013-02-22 22:55:27 -0500

    Document and clean up stream transcriber code.

--- SphinxTest/src/audioStreamTranscriber/StreamTranscriber.java
+++ SphinxTest/src/audioStreamTranscriber/StreamTranscriber.java
@@ -21,7 +21,6 @@ import java.io.PipedOutputStream;
 import java.net.URL;
 
 import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
@@ -29,16 +28,32 @@ import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 
 /**
- * A simple example that shows how to transcribe a continuous audio file that
- * has multiple utterances in it.
+ * A class that uses Sphinx to translate a given <code>AudioInputStream</code>
+ * into an <code>InputStream</code> of the detected words.
  */
 public class StreamTranscriber {
 
-	public static InputStream transcribeStream(AudioInputStream ais)
-			throws IOException, UnsupportedAudioFileException {
+	/**
+	 * Transcribes the given <code>AudioInputStream</code> into an
+	 * <code>InputStream</code> of the transcribed words. The list of words it
+	 * can detect are currently pre-determined, and stroed in the grammar file
+	 * (detectWords.gram) accompanying this class.
+	 * 
+	 * Detection will continue until the source audio stops, at which time the
+	 * produced input stream will end as well. Kicks the detection and stream
+	 * management into a new thread, allowing for this code to be placed in-line
+	 * and the streams to be used as usual.
+	 * 
+	 * @param ais
+	 *            The audio stream to be processed
+	 * @return A stream of detected words, as they are detected.
+	 */
+	public static InputStream transcribeStream(AudioInputStream ais) {
 
+		// Location of Config file
 		URL configURL = StreamTranscriber.class.getResource("config.xml");
 
+		// Configuration manager and recognizer setup
 		ConfigurationManager cm = new ConfigurationManager(configURL);
 		final Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 
@@ -50,24 +65,33 @@ public class StreamTranscriber {
 				.lookup("audioFileDataSource");
 		dataSource.setInputStream(ais, null);
 
+		// Create connected piped input and output streams, for streaming
+		// detected words.
 		PipedInputStream inStream = new PipedInputStream();
-		OutputStream outPipe = new PipedOutputStream(inStream);
+		OutputStream outPipe;
+		try {
+			outPipe = new PipedOutputStream(inStream);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			return null;
+		}
 		final OutputStreamWriter out = new OutputStreamWriter(outPipe);
 
-		// Loop until last utterance in the audio file has been decoded, in
-		// which case the recognizer will return null.
+		// Kick detection into it's own thread, to allow for detection in concurrency with rest of program
 		new Thread(new Runnable() {
-
 			@Override
 			public void run() {
 				try {
 					Result result;
+					// Loop until last utterance in the audio file has been decoded, in
+					// which case the recognizer will return null
 					while ((result = recognizer.recognize()) != null) {
-
 						String resultText = result.getBestResultNoFiller();
+						// Write detected word to output stream
 						out.write(resultText + "\n");
 						out.flush();
 					}
+					// Audio ended, close stream
 					out.flush();
 					out.close();
 				} catch (IOException ioe) {
--- SphinxTest/src/audioStreamTranscriber/sampleDriver/StreamTranscriberDriver.java
+++ SphinxTest/src/audioStreamTranscriber/sampleDriver/StreamTranscriberDriver.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.IllegalFormatException;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -15,35 +14,44 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 import audioStreamTranscriber.StreamTranscriber;
 
-
 public class StreamTranscriberDriver {
 
 	public static void main(String[] args) throws IOException,
 			UnsupportedAudioFileException {
 		URL audioURL;
+		String filename;
 
+		// Set the file to be processed.  The words to be detected need to go into detectWords.gram
 		if (args.length > 0) {
 			audioURL = new File(args[0]).toURI().toURL();
 		} else {
-			// audioURL =Transcriber.class.getResource("10001-90210-01803.wav"); // Base Example
-			// audioURL = Transcriber.class.getResource("out_2_a"); // Our Data
-			audioURL = StreamTranscriberDriver.class.getResource("ritaTest.wav"); // Test Data
+			filename = "10001-90210-01803.wav"; // Base example
+//			filename = "out_2_a"; // Our Sample Data
+//			filename = "ritaTest.wav"; // More clear test data
+			audioURL = StreamTranscriberDriver.class.getResource(filename);
 		}
-		
-		// Base Example
+
+		// Creating of the input stream object
 		AudioInputStream ais;
-		
-		try{
+
+		// Attempt to automagically create ais from the AudioSystem
+		try {
 			ais = AudioSystem.getAudioInputStream(new File(audioURL.getFile()));
-		} catch(IllegalFormatException ife){
+		} catch (UnsupportedAudioFileException uafe) {
+			// If that doesn't work, it's probably our unprocessed audio from
+			// the arduino.
 			AudioFormat pcmFormat = new AudioFormat(9600f, 8, 1, false, false);
-	        ais = new AudioInputStream(new FileInputStream(
-					audioURL.getFile()), pcmFormat, 2000000);
+			ais = new AudioInputStream(new FileInputStream(audioURL.getFile()),
+					pcmFormat, 2000000);
 		}
+
+		// Create a buffered reader from the input stream the transcriber returns
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				StreamTranscriber.transcribeStream(ais)));
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(StreamTranscriber.transcribeStream(ais)));
+		// Print results of the transcribing
 		String line = null;
-		while((line = reader.readLine()) != null){
+		while ((line = reader.readLine()) != null) {
 			System.out.println(line);
 		}
 		System.out.println("--Done Processing--");
