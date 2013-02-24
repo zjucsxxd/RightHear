00be64835e4c1a54d3e193f5f1b263dd6d6032c7
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-02-24 00:12:11 -0500

    Added back code to write to file (for testing)

--- SerialTest/src/SimpleRead.java
+++ SerialTest/src/SimpleRead.java
@@ -1,4 +1,5 @@
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -8,7 +9,7 @@ import jssc.SerialPortException;
 
 public class SimpleRead {
 
-    public static void main(String[] args) throws SerialPortException, IOException {
+    public static void main(String[] args) throws SerialPortException, IOException, InterruptedException {
 //        SerialPort port = new SerialPort("/dev/tty.usbmodemfd121");
     	SerialPort port =  new SerialPort("/dev/tty.usbmodem12341");
         
@@ -20,22 +21,25 @@ public class SimpleRead {
         ProcessingAdapter adapter = new ProcessingAdapter();
         OutputStream toProcess = adapter.getStreamToProcessor();
         
+        FileOutputStream fos = new FileOutputStream("out_ta_2");
         new Thread(new StreamPrinter(adapter.getDetectedWordsStream())).start();
         
         int ii = 0;
         while (true) {
             //wait to make sure we have at least a frame
             if (port.getInputBufferBytesCount() < 256) {
+//                Thread.sleep(1);
                 continue;
             }
-            System.out.println("Data Waiting: " + port.getInputBufferBytesCount());
+//            System.out.println("Data Waiting: " + port.getInputBufferBytesCount());
             byte[] bytes = port.readBytes(256);
             applySilence(bytes);
             toProcess.write(bytes);
-            if ((++ii % 100) == 0) {
-                port.writeInt('A');
-                ii = 0;
-            }
+            fos.write(bytes);
+//            if ((++ii % 100) == 0) {
+//                port.writeInt('A');
+//                ii = 0;
+//            }
         }
     }
 
